768f91a236e94cf2c99371280c695f690f4cad0f
Luigi Freitas Cruz <luigifcruz@me.com>
2020-01-22 23:53:45 -0300

    Ditched PyAudio to fix stability #1

--- src/main/python/demod.py
+++ src/main/python/demod.py
@@ -1,11 +1,11 @@
 from SoapySDR import SOAPY_SDR_RX, SOAPY_SDR_CF32
 import SoapySDR
-import pyaudio
 import queue
 import numpy as np
 from PyQt5.QtCore import QThread
 from utils import toDevice
 from radio.analog import WBFM
+import sounddevice as sd
 
 
 class Demodulator(QThread):
@@ -28,7 +28,6 @@ class Demodulator(QThread):
         self.sdr_buff = 1024
         self.dsp_buff = self.sdr_buff * 4
         self.dsp_out = int(self.dsp_buff/(self.sfs/self.afs))
-
         self.que = queue.Queue()
 
     def activateDevice(self, device):
@@ -40,8 +39,8 @@ class Demodulator(QThread):
         self.sdr.setGainMode(SOAPY_SDR_RX, 0, True)
         self.sdr.setSampleRate(SOAPY_SDR_RX, 0, self.sfs)
         self.sdr.setFrequency(SOAPY_SDR_RX, 0, self.freq)
+        self.rx = self.sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32)
 
-        self.p = pyaudio.PyAudio()
         self.device = str(device)
 
     def setFreq(self, freq):
@@ -56,53 +55,37 @@ class Demodulator(QThread):
         print("[DEMOD] Starting Demodulator")
 
         self.running = True
-        self.mode = 0
-
         buff = np.zeros([self.dsp_buff], dtype=np.complex64)
-        self.rx = self.sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32)
         self.sdr.activateStream(self.rx)
 
-        while self.running:
-            for i in range(self.dsp_buff//self.sdr_buff):
-                self.sdr.readStream(
-                    self.rx,
-                    [buff[(i*self.sdr_buff):]],
-                    self.sdr_buff,
-                    timeoutUs=int(1e9))
-            self.que.put(buff.astype(np.complex64))
+        with sd.OutputStream(blocksize=self.dsp_out, callback=self.director,
+                             samplerate=self.afs, channels=2):
+            while self.running:
+                for i in range(self.dsp_buff//self.sdr_buff):
+                    self.sdr.readStream(self.rx, [buff[(i*self.sdr_buff):]],
+                                        self.sdr_buff, timeoutUs=int(1e9))
+                self.que.put(buff.astype(np.complex64))
 
         self.sdr.deactivateStream(self.rx)
         self.sdr.closeStream(self.rx)
-
         print("[DEMOD] Device stream has stopped.")
+
+    def director(self, outdata, frames, time, status):
+        if self.mode == 0:
+            outdata[:] = np.copy(self.fm())
+        elif self.mode == 1:
+            outdata[:] = np.copy(self.am())
 
     def activateFm(self, tau):
         print("[DEMOD] Setting up FM demodulator...")
-
-        self.running = True
+        self.stereo = True
+        self.demod = WBFM(tau, self.sfs, self.afs,
+                          self.dsp_buff, self.cuda, self.numba)
+        self.que.queue.clear()
         self.mode = 0
 
-        self.stereo = True
-        self.demod = WBFM(tau, self.sfs, self.afs, self.dsp_buff, self.cuda, self.numba)
-
-        stream_info = pyaudio.PaMacCoreStreamInfo(
-            flags=pyaudio.PaMacCoreStreamInfo.paMacCorePlayNice)
-
-        self.que.queue.clear()
-        self.stream = self.p.open(
-            format=pyaudio.paFloat32,
-            channels=2,
-            frames_per_buffer=self.dsp_out,
-            rate=self.afs,
-            output=True,
-            output_host_api_specific_stream_info=stream_info,
-            stream_callback=self.fm)
-
-    def fm(self, in_data, frame_count, time_info, status):
-        # Receive and Demodulate Samples
+    def fm(self):
         L, R = self.demod.run(self.que.get())
-
-        # Create PyAudio Stereo Matrix
         LR = np.zeros((self.dsp_out*2), dtype=np.float32)
 
         if self.demod.freq >= 19015 and self.demod.freq <= 18985:
@@ -112,22 +95,13 @@ class Demodulator(QThread):
             LR[0::2] = L
             LR[1::2] = R
 
-        # Further Sanitize the Output
         LR *= self.vol
-        LR = LR.reshape(self.dsp_out, 2)
-
-        if not self.running or self.mode == 0:
-            print("[DEMOD] Stopping dangling stream.")
-            self.que.queue.clear()
-            return (None, pyaudio.paAbort)
-
-        return (np.copy(LR.tobytes()), pyaudio.paContinue)
+        return LR.reshape(self.dsp_out, 2)
 
     def activateAm(self):
         print("[DEMOD] Setting up AM demodulator...")
-
-        self.running = True
         self.mode = 1
 
-    def am(self, in_data, frame_count, time_info, status):
-        pass
+    def am(self):
+        LR = np.zeros((self.dsp_out*2), dtype=np.float32)
+        return LR.reshape(self.dsp_out, 2)
